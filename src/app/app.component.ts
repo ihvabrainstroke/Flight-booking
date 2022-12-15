1fc18288d71f2e3342d74b3998880d4b22a6a699
ihvabrainstroke <karthikneel2002@gmail.com>
2022-12-15 14:18:32 +0530

    🚀 have  a lot more work to do, just worked on UI rn

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  accounts = [
+    {
+      name: 'Default',
+      status: 'active'
+    },
+  ];
+
+  onAccountAdded(newAccount: {name: string, status: string}) {
+    this.accounts.push(newAccount);
+  }
+
+  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
+    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  }
+}
